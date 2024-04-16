514420ae3f6aeeae263a549fe5088936cae904c2
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-04-16 20:12:54 +0300

    Add more icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    // More icons
+    implementation(libs.androidx.material.icons.extended)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
