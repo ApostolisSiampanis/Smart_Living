6a9a878176c417f4cc04a08e8ede26486d800bbc
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-04-29 16:35:51 +0300

    Update BottomBar to use Screen.kt

--- app/src/main/java/com/aposiamp/smartliving/ui/navigation/BottomBars.kt
+++ app/src/main/java/com/aposiamp/smartliving/ui/navigation/BottomBars.kt
@@ -1,52 +1,28 @@
 package com.aposiamp.smartliving.ui.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.EnergySavingsLeaf
-import androidx.compose.material.icons.filled.PlayCircleFilled
-import androidx.compose.material.icons.filled.ViewComfy
-import androidx.compose.material.icons.outlined.EnergySavingsLeaf
-import androidx.compose.material.icons.outlined.PlayCircleOutline
-import androidx.compose.material.icons.outlined.ViewComfy
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import com.aposiamp.smartliving.R
+import com.aposiamp.smartliving.ui.screen.screensInBottom
 
 @Composable
 fun BottomBar() {
-    val items = listOf(
-        BottomNavigationItem(
-            title = stringResource(id = R.string.devices),
-            selectedIcon = Icons.Filled.ViewComfy,
-            unselectedIcon = Icons.Outlined.ViewComfy,
-            route = "devices"
-        ),
-        BottomNavigationItem(
-            title = stringResource(id = R.string.routines),
-            selectedIcon = Icons.Filled.PlayCircleFilled,
-            unselectedIcon = Icons.Outlined.PlayCircleOutline,
-            route = "routines"
-        ),
-        BottomNavigationItem(
-            title = stringResource(id = R.string.energy),
-            selectedIcon = Icons.Filled.EnergySavingsLeaf,
-            unselectedIcon = Icons.Outlined.EnergySavingsLeaf,
-            route = "energy"
-        )
-    )
-    var selectedItemIndex by rememberSaveable{ mutableStateOf(0) }
+    var selectedItemIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
 
     NavigationBar {
-        items.forEachIndexed { index, item ->
+        screensInBottom.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selectedItemIndex == index,
                 onClick = {
@@ -55,7 +31,7 @@ fun BottomBar() {
                 },
                 label = {
                     Text(
-                        text = item.title,
+                        text = stringResource(id = item.titleResId),
                         fontFamily = FontFamily(Font(R.font.carlito_regular))
                     )
                 },
@@ -66,7 +42,7 @@ fun BottomBar() {
                         } else {
                             item.unselectedIcon
                         },
-                        contentDescription = item.title
+                        contentDescription = stringResource(id = item.titleResId)
                     )
                 }
             )
