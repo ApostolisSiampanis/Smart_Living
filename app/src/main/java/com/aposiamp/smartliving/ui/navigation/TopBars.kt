a25dafe4c43680c1a4b09ef807006ac3eaf39693
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-04-29 12:56:54 +0300

    Create TopBars.kt

---
+++ app/src/main/java/com/aposiamp/smartliving/ui/navigation/TopBars.kt
@@ -0,0 +1,58 @@
+package com.aposiamp.smartliving.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.aposiamp.smartliving.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuMediumTopAppBar(
+    title: String,
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    scrollBehavior: TopAppBarScrollBehavior
+) {
+    MediumTopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontFamily = FontFamily(Font(R.font.carlito_regular))
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                scope.launch {
+                    drawerState.open()
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/}) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add a Device or a Scenario"
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
