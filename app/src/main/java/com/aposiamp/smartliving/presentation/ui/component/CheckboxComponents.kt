9c45ed74b910c2d95eea7deacf8ec817441e403a
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-22 22:42:28 +0300

    Rename CheckboxComponent to TermsCheckboxComponent

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/CheckboxComponents.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/CheckboxComponents.kt
@@ -1,13 +1,14 @@
 package com.aposiamp.smartliving.presentation.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,30 +16,42 @@ import com.aposiamp.smartliving.presentation.ui.theme.PrussianBlue
 import com.aposiamp.smartliving.presentation.ui.theme.SkyBlue
 
 @Composable
-fun CheckboxComponent(
-    onTextSelected: (String) -> Unit
+fun TermsCheckboxComponent(
+    checkedValue: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onTextSelected: (String) -> Unit,
+    errorMessageValue: String,
+    errorStatus: Boolean = false
 ) {
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(56.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .fillMaxSize()
     ) {
-        val checkedState = remember {
-            mutableStateOf(false)
-        }
-
-        Checkbox(
-            checked = checkedState.value,
-            onCheckedChange = {
-                checkedState.value != checkedState.value
-            },
-            colors = CheckboxDefaults.colors(
-                checkmarkColor = PrussianBlue,
-                checkedColor = SkyBlue
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(56.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Checkbox(
+                checked = checkedValue,
+                onCheckedChange = onCheckedChange,
+                colors = CheckboxDefaults.colors(
+                    checkmarkColor = PrussianBlue,
+                    checkedColor = SkyBlue
+                )
             )
-        )
-
-        PolicyAndTermsClickableTextComponent(onTextSelected)
+            PolicyAndTermsClickableTextComponent(onTextSelected)
+        }
+        if (errorStatus) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                ErrorTextComponent(value = errorMessageValue)
+            }
+        }
     }
 }
