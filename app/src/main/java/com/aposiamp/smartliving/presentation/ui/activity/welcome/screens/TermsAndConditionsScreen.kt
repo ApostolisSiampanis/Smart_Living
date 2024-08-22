aa9b3321c3623e183615103dd4234599b2ac641e
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-22 14:47:25 +0300

    TermsAndConditionsScreen first version, without text

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/welcome/screens/TermsAndConditionsScreen.kt
@@ -0,0 +1,41 @@
+package com.aposiamp.smartliving.presentation.ui.activity.welcome.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun TermsAndConditionsScreen(
+    navController: NavController
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+
+        },
+        content = { padding ->
+            Surface(
+                color = Color.White,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 28.dp, end = 28.dp, top = 18.dp)
+                ) {
+
+                }
+            }
+        }
+    )
+}
