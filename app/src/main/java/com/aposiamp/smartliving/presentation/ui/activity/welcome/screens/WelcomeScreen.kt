f991e970b72f96d8d76f78345593f2b81635bcfe
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-23 01:22:02 +0300

    Add smart_living_logo.xml to WelcomeScreen

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/welcome/screens/WelcomeScreen.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/welcome/screens/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.aposiamp.smartliving.presentation.ui.activity.welcome.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,17 +10,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.aposiamp.smartliving.R
 import com.aposiamp.smartliving.presentation.ui.component.GeneralButtonComponent
+import com.aposiamp.smartliving.presentation.ui.component.GeneralTextComponent
 
 @Composable
 fun WelcomeScreen(
@@ -37,16 +38,17 @@ fun WelcomeScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             item {
-                //TODO: Add App Logo
+                Image(
+                    painter = painterResource(id = R.drawable.smart_living_logo),
+                    contentDescription = stringResource(id = R.string.smart_living_logo)
+                )
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = stringResource(id = R.string.welcome_text),
-                    textAlign = TextAlign.Center
+                GeneralTextComponent(
+                    value = stringResource(id = R.string.welcome_text),
                 )
                 Spacer(modifier = Modifier.height(60.dp))
-                Text(
-                    text = stringResource(id = R.string.creator),
-                    textAlign = TextAlign.Center
+                GeneralTextComponent(
+                    value = stringResource(id = R.string.creator),
                 )
 
                 Column(
