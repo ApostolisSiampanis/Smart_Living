183fe3bac6de13f92b379acc9bc7c396545a70ce
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-23 01:25:54 +0300

    Create LoginFormEvent and LoginFormState

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/state/auth/LoginFormState.kt
@@ -0,0 +1,9 @@
+package com.aposiamp.smartliving.presentation.ui.state.auth
+
+data class LoginFormState(
+    val email: String = "",
+    val emailError: String? = null,
+    val password: String = "",
+    val passwordError: String? = null,
+    var errorMessage: String? = null
+)
