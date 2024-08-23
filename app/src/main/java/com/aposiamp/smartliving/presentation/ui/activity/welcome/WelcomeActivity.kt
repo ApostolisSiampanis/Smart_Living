8529dc43232b178eaf9f35dbe79c311f1877a9d3
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-23 19:53:00 +0300

    Update WelcomeActivity to create instances of LoginViewModel and SignUpViewModel

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/welcome/WelcomeActivity.kt
@@ -7,8 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aposiamp.smartliving.SmartLiving
 import com.aposiamp.smartliving.presentation.ui.activity.welcome.navigation.WelcomeNavigation
 import com.aposiamp.smartliving.presentation.ui.theme.SmartLivingTheme
+import com.aposiamp.smartliving.presentation.viewmodel.viewModelFactory
+import com.aposiamp.smartliving.presentation.viewmodel.welcome.auth.LoginViewModel
+import com.aposiamp.smartliving.presentation.viewmodel.welcome.auth.SignUpViewModel
 
 class WelcomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +24,33 @@ class WelcomeActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WelcomeNavigation()
+                    val loginViewModel = viewModel<LoginViewModel>(
+                        factory = viewModelFactory {
+                            LoginViewModel(
+                                loginUseCase = SmartLiving.appModule.loginUseCase,
+                                validateEmail = SmartLiving.appModule.validateEmail,
+                                validatePassword = SmartLiving.appModule.validatePassword
+                            )
+                        }
+                    )
+
+                    val signUpViewModel = viewModel<SignUpViewModel>(
+                        factory = viewModelFactory {
+                            SignUpViewModel(
+                                signUpUseCase = SmartLiving.appModule.signUpUseCase,
+                                validateEmail = SmartLiving.appModule.validateEmail,
+                                validatePassword = SmartLiving.appModule.validatePassword,
+                                validateFirstName = SmartLiving.appModule.validateFirstName,
+                                validateLastName = SmartLiving.appModule.validateLastName,
+                                validateTerms = SmartLiving.appModule.validateTerms
+                            )
+                        }
+                    )
+
+                    WelcomeNavigation(
+                        loginViewModel = loginViewModel,
+                        signUpViewModel = signUpViewModel
+                    )
                 }
             }
         }
