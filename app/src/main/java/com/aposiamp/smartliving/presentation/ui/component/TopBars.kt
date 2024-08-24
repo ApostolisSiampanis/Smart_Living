3bf79cd10c2f1ce1d9d6bebe9c1ee7a84063c9c7
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-24 20:38:55 +0300

    Create BackAppTopBar

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/TopBars.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/TopBars.kt
@@ -1,5 +1,6 @@
 package com.aposiamp.smartliving.presentation.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
@@ -9,8 +10,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import com.aposiamp.smartliving.R
@@ -53,5 +59,35 @@ fun MenuMediumTopAppBar(
             }
         },
         scrollBehavior = scrollBehavior
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BackAppTopBar(
+    title: String,
+    color: Color,
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            HeaderText(value = title)
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    onBackClick()
+                },
+                content = {
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow_back),
+                        contentDescription = stringResource(id = R.string.go_back)
+                    )
+                }
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = color
+        )
     )
 }
