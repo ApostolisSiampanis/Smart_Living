41353eb3532963ecacf3f2f52e0ad029ed3f8312
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-27 14:10:56 +0300

    Create DateUtils to  get the timestamp in UTC format

---
+++ app/src/main/java/com/aposiamp/smartliving/utils/DateUtils.kt
@@ -0,0 +1,13 @@
+package com.aposiamp.smartliving.utils
+
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+object DateUtils {
+    fun getCurrentDateUTC(): String {
+        val now = Instant.now()
+        val formatter = DateTimeFormatter.ISO_INSTANT.withZone(ZoneId.of("UTC"))
+        return formatter.format(now)
+    }
+}
