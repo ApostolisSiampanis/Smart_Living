ff16aa6a084ab26c65abbb36f7e8ef9b8c33a4a5
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-29 15:23:24 +0300

    Rename NavigationDrawerUiItem.kt to NavigationUiItem to be more abstract

--- app/src/main/java/com/aposiamp/smartliving/presentation/model/NavigationUiItem.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/model/NavigationUiItem.kt
@@ -2,7 +2,7 @@ package com.aposiamp.smartliving.presentation.model
 
 import androidx.annotation.StringRes
 
-data class NavigationDrawerUiItem(
+data class NavigationUiItem(
     @StringRes val titleResId: Int,
     val unselectedIcon: Int,
     val selectedIcon: Int? = null,
