028fea8793eae3db6f61d129872b2314b2339dcf
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-29 15:05:38 +0300

    Update NavigationDrawerRepositoryImpl

--- app/src/main/java/com/aposiamp/smartliving/data/repository/NavigationDrawerRepositoryImpl.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/repository/NavigationDrawerRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.aposiamp.smartliving.data.repository
 
+import androidx.compose.ui.res.stringResource
 import com.aposiamp.smartliving.R
 import com.aposiamp.smartliving.domain.repository.NavigationDrawerRepository
 import com.aposiamp.smartliving.presentation.model.NavigationDrawerUiItem
@@ -7,7 +8,7 @@ import com.aposiamp.smartliving.presentation.model.NavigationDrawerUiItem
 class NavigationDrawerRepositoryImpl : NavigationDrawerRepository {
     override fun getNavigationDrawerItems() = listOf(
         NavigationDrawerUiItem(
-            title = "Settings",
+            titleResId = R.string.settings,
             unselectedIcon = R.drawable.settings_outlined,
             selectedIcon = R.drawable.settings_outlined,
             route = "settings"
