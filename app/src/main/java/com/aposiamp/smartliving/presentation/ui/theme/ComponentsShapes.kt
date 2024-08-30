c17b3f4b04652b65198a79f146d3c71f9db4a4cf
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-30 22:51:03 +0300

    Update componentShapes

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/theme/ComponentsShapes.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/theme/ComponentsShapes.kt
@@ -5,8 +5,9 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val componentShapes = Shapes(
+    extraSmall = RoundedCornerShape(0.dp),
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(8.dp),
     large = RoundedCornerShape(16.dp),
-    extraLarge = RoundedCornerShape(30.dp)
+    extraLarge = RoundedCornerShape(100.dp)
 )
