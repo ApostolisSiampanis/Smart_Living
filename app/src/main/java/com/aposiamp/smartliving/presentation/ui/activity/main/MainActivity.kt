127c9923ee3fe119349916c663b71574f5449a13
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-30 23:01:32 +0300

    Update MainActivity

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/MainActivity.kt
@@ -3,14 +3,42 @@ package com.aposiamp.smartliving.presentation.ui.activity.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.aposiamp.smartliving.SmartLiving
+import com.aposiamp.smartliving.presentation.ui.activity.main.navigation.MainNavigation
 import com.aposiamp.smartliving.presentation.ui.theme.SmartLivingTheme
+import com.aposiamp.smartliving.presentation.viewmodel.main.DevicesViewModel
+import com.aposiamp.smartliving.presentation.viewmodel.main.MainSharedViewModel
+import com.aposiamp.smartliving.presentation.viewmodel.viewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SmartLivingTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val devicesViewModel = viewModel<DevicesViewModel>()
 
+                    val mainSharedViewModel = viewModel<MainSharedViewModel>(
+                        factory = viewModelFactory {
+                            MainSharedViewModel(
+                                getEnvironmentalDataUseCase = SmartLiving.appModule.getEnvironmentalDataUseCase
+                            )
+                        }
+                    )
+
+                    MainNavigation(
+                        devicesViewModel = devicesViewModel,
+                        mainSharedViewModel = mainSharedViewModel
+                    )
+                }
             }
         }
     }
