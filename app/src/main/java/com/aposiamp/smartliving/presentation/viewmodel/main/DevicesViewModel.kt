0f77714742ecaa6091768d473caa9c84c289dd68
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-30 22:48:24 +0300

    Create version 1 of DevicesViewModel

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/DevicesViewModel.kt
@@ -0,0 +1,12 @@
+package com.aposiamp.smartliving.presentation.viewmodel.main
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aposiamp.smartliving.domain.usecase.sensor.GetEnvironmentalDataUseCase
+import kotlinx.coroutines.launch
+
+class DevicesViewModel(
+) : ViewModel() {
+
+}
