d9bd31187436c783bfb51c200f5d0c2ad2d60bf6
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-08-30 23:00:28 +0300

    Create EnvironmentalSensorRepositoryImpl

---
+++ app/src/main/java/com/aposiamp/smartliving/data/repository/EnvironmentalSensorRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.aposiamp.smartliving.data.repository
+
+import com.aposiamp.smartliving.data.model.EnvironmentalDataDTO
+import com.aposiamp.smartliving.data.source.local.EnvironmentalSensorDataSource
+import com.aposiamp.smartliving.data.source.remote.FirestoreDataSource
+import com.aposiamp.smartliving.domain.repository.EnvironmentalSensorRepository
+import com.aposiamp.smartliving.utils.DateUtils
+
+class EnvironmentalSensorRepositoryImpl(
+    private val environmentalSensorDataSource: EnvironmentalSensorDataSource,
+    private val firestoreDataSource: FirestoreDataSource
+) : EnvironmentalSensorRepository {
+    override suspend fun getEnvironmentalData(): EnvironmentalDataDTO {
+        return EnvironmentalDataDTO(
+            temperature = environmentalSensorDataSource.getTemperatureData(),
+            humidity = environmentalSensorDataSource.getHumidityData(),
+            timestamp = DateUtils.getCurrentDateUTC()
+        )
+    }
+
+    override suspend fun setEnvironmentalData(uid: String, environmentalDataDTO: EnvironmentalDataDTO) {
+        firestoreDataSource.setEnvironmentalData(uid, environmentalDataDTO)
+    }
+}
