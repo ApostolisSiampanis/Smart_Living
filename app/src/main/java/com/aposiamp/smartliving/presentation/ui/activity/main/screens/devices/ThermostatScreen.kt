9d8f32d8abd1f5531875ae924440eefc3a882a03
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-01 21:52:42 +0300

    Small changes to use DeviceIndicatorCard into ThermostatScreen

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/screens/devices/ThermostatScreen.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/screens/devices/ThermostatScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -73,7 +74,11 @@ fun ThermostatScreen(
                                 .fillMaxWidth(),
                             horizontalArrangement = Arrangement.Center
                         ) {
-                            DeviceIndicatorCard {
+                            DeviceIndicatorCard(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.8f)
+                                    .aspectRatio(1f)
+                            ) {
                                 DeviceCircularIndicator(
                                     modifier = Modifier
                                         .fillMaxSize()
