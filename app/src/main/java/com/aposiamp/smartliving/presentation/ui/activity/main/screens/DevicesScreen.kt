37a53be06b68ea0c986f461f9e02456c5012f51c
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-02 15:26:14 +0300

    Update DevicesScreen to use the updated version of MenuMediumTopAppBar

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/screens/DevicesScreen.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/screens/DevicesScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,7 +22,7 @@ import com.aposiamp.smartliving.presentation.ui.component.BottomBar
 import com.aposiamp.smartliving.presentation.ui.component.MenuMediumTopAppBar
 import com.aposiamp.smartliving.presentation.ui.component.NavigationDrawer
 import com.aposiamp.smartliving.presentation.viewmodel.main.DevicesViewModel
-import com.aposiamp.smartliving.presentation.viewmodel.main.MainSharedViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,7 +32,6 @@ fun DevicesScreen(
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     val drawerState = rememberDrawerState(DrawerValue.Closed)
-    //to be changed
     val scope = rememberCoroutineScope()
 
     ModalNavigationDrawer(
@@ -50,11 +50,20 @@ fun DevicesScreen(
                 .fillMaxSize()
                 .nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
-                //to be changed
+                //TODO: to be changed, use the name of the "house"
                 MenuMediumTopAppBar(
                     title = "My Home",
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    onMenuClick = {
+                        scope.launch {
+                            if (drawerState.isOpen) {
+                                drawerState.close()
+                            } else {
+                                drawerState.open()
+                            }
+                        }
+                    },
                     drawerState = drawerState,
-                    scope = scope,
                     scrollBehavior = scrollBehavior
                 )
             },
