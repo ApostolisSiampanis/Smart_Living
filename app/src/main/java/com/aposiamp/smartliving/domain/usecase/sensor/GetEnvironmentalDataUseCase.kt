dd57e65160e55a73f393c4a849b51dad869460f7
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-04 22:01:51 +0300

    Update GetEnvironmentalDataUseCase, use of EnvironmentalDataMapper

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/sensor/GetEnvironmentalDataUseCase.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/sensor/GetEnvironmentalDataUseCase.kt
@@ -1,6 +1,7 @@
 package com.aposiamp.smartliving.domain.usecase.sensor
 
 import com.aposiamp.smartliving.data.model.EnvironmentalDataDTO
+import com.aposiamp.smartliving.domain.mapper.EnvironmentalDataMapper
 import com.aposiamp.smartliving.domain.model.EnvironmentalData
 import com.aposiamp.smartliving.domain.repository.EnvironmentalSensorRepository
 import com.aposiamp.smartliving.domain.usecase.user.GetCurrentUserUseCase
@@ -10,20 +11,15 @@ class GetEnvironmentalDataUseCase(
     private val getCurrentUserUseCase: GetCurrentUserUseCase
 ) {
     suspend fun execute(): EnvironmentalData {
-        val environmentalData = EnvironmentalData(
-            temperature = environmentalSensorRepository.getEnvironmentalData().temperature,
-            humidity = environmentalSensorRepository.getEnvironmentalData().humidity,
-            timestamp = environmentalSensorRepository.getEnvironmentalData().timestamp
-        )
+        val environmentalDataDTO = environmentalSensorRepository.getEnvironmentalData()
+        val environmentalData = EnvironmentalDataMapper.fromDto(environmentalDataDTO)
+
         val currentUser = getCurrentUserUseCase.execute()
         val uid = currentUser?.uid ?: throw Exception("User not logged in")
+
         environmentalSensorRepository.setEnvironmentalData(
             uid,
-            EnvironmentalDataDTO(
-                temperature = environmentalData.temperature,
-                humidity = environmentalData.humidity,
-                timestamp = environmentalData.timestamp
-            )
+            EnvironmentalDataMapper.toDto(environmentalData)
         )
         return environmentalData
     }
