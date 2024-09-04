0bd8f5d489459bca60e5548d1d72d799e6243e87
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-04 19:50:53 +0300

    Delete NavigationDrawerRepository.kt

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/NavigationDrawerRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.aposiamp.smartliving.domain.repository
-
-import com.aposiamp.smartliving.presentation.model.NavigationUiItem
-
-interface NavigationDrawerRepository {
-    fun getNavigationDrawerItems(): List<NavigationUiItem>
-}
