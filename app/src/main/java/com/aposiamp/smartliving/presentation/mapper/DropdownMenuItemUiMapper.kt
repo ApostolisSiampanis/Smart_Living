6038633cd4e7bcefc0db17897e020878153d3d42
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-04 22:24:07 +0300

    Create DropdownMenuItemUiMapper

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/mapper/DropdownMenuItemUiMapper.kt
@@ -0,0 +1,23 @@
+package com.aposiamp.smartliving.presentation.mapper
+
+import androidx.navigation.NavController
+import com.aposiamp.smartliving.domain.model.DropdownMenuItemData
+import com.aposiamp.smartliving.presentation.model.DropdownMenuItemUiModel
+
+object DropdownMenuItemUiMapper {
+    private fun toUiModel(domain: DropdownMenuItemData, navController: NavController): DropdownMenuItemUiModel {
+        val onClick: () -> Unit = when (domain.text) {
+            "Add a New Room" -> { { navController.navigate("") } } //TODO: Add the correct route
+            "Add a New Device" -> { { navController.navigate("") }} //TODO: Add the correct route
+            else -> { { } }
+        }
+        return DropdownMenuItemUiModel(
+            text = domain.text,
+            onClick = onClick
+        )
+    }
+
+    fun List<DropdownMenuItemData>.toUiModelList(navController: NavController): List<DropdownMenuItemUiModel> {
+        return this.map { toUiModel(it, navController) }
+    }
+}
