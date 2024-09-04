647499a95d8db5f29ee12577658ca0532dba1eea
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-04 22:24:14 +0300

    Create NavigationItemUiMapper

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/mapper/NavigationItemUiMapper.kt
@@ -0,0 +1,68 @@
+package com.aposiamp.smartliving.presentation.mapper
+
+import com.aposiamp.smartliving.R
+import com.aposiamp.smartliving.domain.model.NavigationItemData
+import com.aposiamp.smartliving.presentation.model.NavigationItemUiModel
+
+object NavigationItemUiMapper {
+    private fun toUiModel(domain: NavigationItemData): NavigationItemUiModel {
+        val titleResId = when (domain.title) {
+            // Bottom Menu
+            "Devices" -> R.string.devices
+            "Routines" -> R.string.routines
+            "Energy" -> R.string.energy
+            // Navigation Drawer
+            "Settings" -> R.string.settings
+            "About" -> R.string.about
+            "Logout" -> R.string.logout
+            else -> 0
+        }
+
+        val selectedIcon = when (domain.title) {
+            // Bottom Menu
+            "Devices" -> R.drawable.devices_filled
+            "Routines" -> R.drawable.routines_filled
+            "Energy" -> R.drawable.energy_filled
+            // Navigation Drawer
+            "Settings" -> R.drawable.settings_filled
+            "About" -> R.drawable.about_filled
+            "Logout" -> R.drawable.logout
+            else -> null
+        }
+
+        val unSelectedIcon = when (domain.title) {
+            // Bottom Menu
+            "Devices" -> R.drawable.devices_outlined
+            "Routines" -> R.drawable.routines_outlined
+            "Energy" -> R.drawable.energy_outlined
+            // Navigation Drawer
+            "Settings" -> R.drawable.settings_outlined
+            "About" -> R.drawable.about_outlined
+            "Logout" -> R.drawable.logout
+            else -> null
+        }
+
+        val route = when (domain.title) {
+            // Bottom Menu
+            "Devices" -> "devices"
+            "Routines" -> "routines"
+            "Energy" -> "energy"
+            // Navigation Drawer
+            "Settings" -> "settings"
+            "About" -> "about"
+            "Logout" -> null
+            else -> null
+        }
+
+        return NavigationItemUiModel(
+            titleResId = titleResId,
+            unselectedIcon = unSelectedIcon,
+            selectedIcon = selectedIcon,
+            route = route
+        )
+    }
+
+    fun List<NavigationItemData>.toUiModelList(): List<NavigationItemUiModel> {
+        return this.map { toUiModel(it) }
+    }
+}
