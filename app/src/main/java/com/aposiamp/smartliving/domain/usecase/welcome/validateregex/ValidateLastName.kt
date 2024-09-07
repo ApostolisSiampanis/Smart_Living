d3613fa5e99066b6cc74dc0486cb5fe167b766d7
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-07 13:44:33 +0300

    Update ValidateLastName

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/validateregex/ValidateLastName.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/validateregex/ValidateLastName.kt
@@ -1,18 +1,21 @@
 package com.aposiamp.smartliving.domain.usecase.welcome.validateregex
 
-class ValidateLastName {
+import android.content.Context
+import com.aposiamp.smartliving.R
+
+class ValidateLastName(private val context: Context) {
     fun execute(lastName: String): ValidateResult {
         if (lastName.isBlank()) {
             return ValidateResult(
                 successful = false,
-                errorMessage = "The firstname can't be blank"
+                errorMessage = context.getString(R.string.the_last_name_can_not_be_blank)
             )
         }
         val allLetters = lastName.all { it.isLetter() }
         if (!allLetters) {
             return ValidateResult(
                 successful = false,
-                errorMessage = "The firstname can only contain letters"
+                errorMessage = context.getString(R.string.the_last_name_can_only_contain_letters)
             )
         }
         return ValidateResult(
