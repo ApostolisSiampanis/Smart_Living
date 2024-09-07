196a0af5d01b37a86cda67e915b1f9bc122814d7
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-07 13:44:44 +0300

    Update ValidateTerms

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/validateregex/ValidateTerms.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/validateregex/ValidateTerms.kt
@@ -1,11 +1,14 @@
 package com.aposiamp.smartliving.domain.usecase.welcome.validateregex
 
-class ValidateTerms {
+import android.content.Context
+import com.aposiamp.smartliving.R
+
+class ValidateTerms(private val context: Context) {
     fun execute(acceptedTerms: Boolean): ValidateResult {
         if (!acceptedTerms) {
             return ValidateResult(
                 successful = false,
-                errorMessage = "You need to accept the terms"
+                errorMessage = context.getString(R.string.you_need_to_accept_the_terms)
             )
         }
         return ValidateResult(
