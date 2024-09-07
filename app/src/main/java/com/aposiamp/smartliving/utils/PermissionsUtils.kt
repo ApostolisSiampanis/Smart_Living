784eaafb59127d7c64f326692a24fe9ebbfa0425
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-07 11:13:18 +0300

    Rename AppSettingsUtils to PermissionsUtils and add areAllPermissionsGranted method

--- app/src/main/java/com/aposiamp/smartliving/utils/PermissionsUtils.kt
+++ app/src/main/java/com/aposiamp/smartliving/utils/PermissionsUtils.kt
@@ -5,7 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 
-object AppSettingsUtils {
+object PermissionsUtils {
+    fun areAllPermissionsGranted(context: Context, permissions: Array<String>): Boolean {
+        return permissions.all { permission ->
+            context.checkSelfPermission(permission) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        }
+    }
+
     fun openAppSettings(context: Context) {
         val intent = Intent().apply {
             action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
