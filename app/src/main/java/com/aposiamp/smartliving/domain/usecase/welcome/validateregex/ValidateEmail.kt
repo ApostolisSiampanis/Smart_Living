183edc6fa29e6d9e5abb3cbf21bc5d2d205176df
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-07 13:44:18 +0300

    Update ValidateEmail

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/validateregex/ValidateEmail.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/validateregex/ValidateEmail.kt
@@ -1,19 +1,21 @@
 package com.aposiamp.smartliving.domain.usecase.welcome.validateregex
 
+import android.content.Context
 import android.util.Patterns
+import com.aposiamp.smartliving.R
 
-class ValidateEmail {
+class ValidateEmail(private val context: Context) {
     fun execute(email: String): ValidateResult {
         if (email.isBlank()) {
             return ValidateResult(
                 successful = false,
-                errorMessage = "The email can't be blank"
+                errorMessage = context.getString(R.string.the_email_can_not_be_blank)
             )
         }
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             return ValidateResult(
                 successful = false,
-                errorMessage = "That's not a valid email"
+                errorMessage = context.getString(R.string.that_is_not_a_valid_email)
             )
         }
         return ValidateResult(
