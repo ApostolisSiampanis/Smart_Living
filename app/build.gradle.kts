9c4d86c4f7a44679bb75e7367f88292de0e2a918
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-10 17:40:07 +0300

    Add firebase realtime database dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.database)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
