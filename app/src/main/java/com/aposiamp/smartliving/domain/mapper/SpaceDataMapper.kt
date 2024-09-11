91bfa3d6f917d4b966dba88fd12cc1386c1be61f
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-11 17:32:33 +0300

    Update SpaceDataMapper, create fromDto

--- app/src/main/java/com/aposiamp/smartliving/domain/mapper/SpaceDataMapper.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/mapper/SpaceDataMapper.kt
@@ -4,6 +4,13 @@ import com.aposiamp.smartliving.data.model.SpaceDataDTO
 import com.aposiamp.smartliving.domain.model.SpaceData
 
 object SpaceDataMapper {
+    fun fromDto(dto: SpaceDataDTO): SpaceData {
+        return SpaceData(
+            spaceName = dto.spaceName,
+            rooms = listOf()
+        )
+    }
+
     fun toDto(domain: SpaceData): SpaceDataDTO {
         return SpaceDataDTO(
             spaceName = domain.spaceName
