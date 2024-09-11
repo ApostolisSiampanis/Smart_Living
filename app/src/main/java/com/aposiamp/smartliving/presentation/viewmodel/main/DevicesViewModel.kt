6e82f1ba27763c42f6e2d0fd95a3b578589274b9
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-11 17:33:03 +0300

    Update DevicesViewModel to fetch the space name of the user

--- app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/DevicesViewModel.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/DevicesViewModel.kt
@@ -1,8 +1,26 @@
 package com.aposiamp.smartliving.presentation.viewmodel.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aposiamp.smartliving.domain.usecase.main.GetDevicesSpaceNameUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class DevicesViewModel(
+    private val getDevicesSpaceNameUseCase: GetDevicesSpaceNameUseCase
 ) : ViewModel() {
+    private val _spaceName = MutableStateFlow("")
+    val spaceName: StateFlow<String> = _spaceName
 
+    init {
+        fetchSpaceName()
+    }
+
+    private fun fetchSpaceName() {
+        viewModelScope.launch {
+            val spaceData = getDevicesSpaceNameUseCase.execute()
+            _spaceName.value = spaceData.spaceName.toString()
+        }
+    }
 }
