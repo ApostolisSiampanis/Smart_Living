4a182499ee745bade1f61c78daec36c33d2c0e79
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-11 17:29:16 +0300

    Update DeviceAndSpaceRepository, create getDevicesSpaceName

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceAndSpaceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceAndSpaceRepository.kt
@@ -4,5 +4,6 @@ import com.aposiamp.smartliving.data.model.SpaceDataDTO
 
 interface DeviceAndSpaceRepository {
     suspend fun setDevicesSpaceData(userId: String, spaceDataDTO: SpaceDataDTO)
+    suspend fun getDevicesSpaceName(userId: String): SpaceDataDTO
     suspend fun checkIfSpaceDataExists(userId: String): Boolean
 }
