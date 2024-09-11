025067723575029176e865caad1c82c684839b13
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-11 19:33:22 +0300

    Update MainSharedViewModel, add and use SetEnvironmentalDataUseCase

--- app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/MainSharedViewModel.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/MainSharedViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aposiamp.smartliving.domain.usecase.sensor.GetEnvironmentalDataUseCase
+import com.aposiamp.smartliving.domain.usecase.sensor.SetEnvironmentalDataUseCase
 import com.aposiamp.smartliving.presentation.mapper.EnvironmentalDataUiMapper
 import com.aposiamp.smartliving.presentation.model.EnvironmentalDataUiModel
 import kotlinx.coroutines.launch
 
 class MainSharedViewModel(
-    private val getEnvironmentalDataUseCase: GetEnvironmentalDataUseCase
+    private val getEnvironmentalDataUseCase: GetEnvironmentalDataUseCase,
+    private val setEnvironmentalDataUseCase: SetEnvironmentalDataUseCase
 ) : ViewModel() {
     private val _environmentalData = MutableLiveData<EnvironmentalDataUiModel>()
     val environmentalData: LiveData<EnvironmentalDataUiModel> = _environmentalData
@@ -18,6 +20,7 @@ class MainSharedViewModel(
     init {
         viewModelScope.launch {
             val environmentalData = getEnvironmentalDataUseCase.execute()
+            setEnvironmentalDataUseCase.execute(environmentalData)
             _environmentalData.value = EnvironmentalDataUiMapper.fromDomain(environmentalData)
         }
     }
