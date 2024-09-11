7ebbaec16bcfbdc2153f5a2a15f057861e16c313
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-11 12:34:24 +0300

    Update DeviceAndSpaceRepository, create checkIfSpaceDataExists

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceAndSpaceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceAndSpaceRepository.kt
@@ -4,4 +4,5 @@ import com.aposiamp.smartliving.data.model.SpaceDataDTO
 
 interface DeviceAndSpaceRepository {
     suspend fun setDevicesSpaceData(userId: String, spaceDataDTO: SpaceDataDTO)
+    suspend fun checkIfSpaceDataExists(userId: String): Boolean
 }
