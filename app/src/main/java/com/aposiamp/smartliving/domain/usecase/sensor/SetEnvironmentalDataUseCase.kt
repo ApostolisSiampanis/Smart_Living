0caabd24cb4cab6b536ba2f931aa8a7786951ec2
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-11 19:32:18 +0300

    Create SetEnvironmentalDataUseCase

---
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/sensor/SetEnvironmentalDataUseCase.kt
@@ -0,0 +1,21 @@
+package com.aposiamp.smartliving.domain.usecase.sensor
+
+import com.aposiamp.smartliving.domain.mapper.EnvironmentalDataMapper
+import com.aposiamp.smartliving.domain.model.EnvironmentalData
+import com.aposiamp.smartliving.domain.repository.EnvironmentalSensorRepository
+import com.aposiamp.smartliving.domain.usecase.user.GetCurrentUserUseCase
+
+class SetEnvironmentalDataUseCase(
+    private val environmentalSensorRepository: EnvironmentalSensorRepository,
+    private val getCurrentUserUseCase: GetCurrentUserUseCase
+) {
+    suspend fun execute(environmentalData: EnvironmentalData) {
+        val currentUser = getCurrentUserUseCase.execute()
+        val uid = currentUser?.uid ?: throw Exception("User not logged in")
+
+        environmentalSensorRepository.setEnvironmentalData(
+            uid,
+            EnvironmentalDataMapper.toDto(environmentalData)
+        )
+    }
+}
