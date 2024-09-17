e46033fdfc04d6221b83bf10b47c358be49c8518
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 19:02:42 +0300

    Update CreateSpaceFormEvent

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/event/CreateSpaceFormEvent.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/event/CreateSpaceFormEvent.kt
@@ -2,5 +2,7 @@ package com.aposiamp.smartliving.presentation.ui.event
 
 sealed class CreateSpaceFormEvent{
     data class SpaceNameChanged(val spaceName: String) : CreateSpaceFormEvent()
-    object Submit : CreateSpaceFormEvent()
+    data class SpaceAddressChanged(val spaceAddress: String) : CreateSpaceFormEvent()
+    data class PlaceIdSelected(val placeId: String) : CreateSpaceFormEvent()
+    data object Submit : CreateSpaceFormEvent()
 }
