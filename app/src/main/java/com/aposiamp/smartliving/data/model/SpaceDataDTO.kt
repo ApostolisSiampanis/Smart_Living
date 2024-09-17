9a9ecffc90a8c783db92fa9fc05b1ab830cf4c32
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 18:18:30 +0300

    Update SpaceDataDTO

--- app/src/main/java/com/aposiamp/smartliving/data/model/SpaceDataDTO.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/model/SpaceDataDTO.kt
@@ -3,5 +3,8 @@ package com.aposiamp.smartliving.data.model
 import com.google.firebase.database.PropertyName
 
 data class SpaceDataDTO(
-    @get:PropertyName("space_name") @set:PropertyName("space_name") var spaceName: String? = null
+    @get:PropertyName("placeId") @set:PropertyName("placeId") var placeId: String? = null,
+    @get:PropertyName("space_name") @set:PropertyName("space_name") var spaceName: String? = null,
+    @get:PropertyName("full_address") @set:PropertyName("full_address") var fullAddress: String? = null,
+    @get:PropertyName("location") @set:PropertyName("location") var location: LocationDataDTO? = null
 )
