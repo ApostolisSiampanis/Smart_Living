d0bbc815f645f2503bc4b09888baec52bfe4c38c
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 19:00:08 +0300

    Rename GetDevicesSpaceNameUseCase to GetSpaceNameUseCase

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/main/GetSpaceNameUseCase.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/main/GetSpaceNameUseCase.kt
@@ -2,18 +2,18 @@ package com.aposiamp.smartliving.domain.usecase.main
 
 import com.aposiamp.smartliving.domain.mapper.SpaceDataMapper
 import com.aposiamp.smartliving.domain.model.SpaceData
-import com.aposiamp.smartliving.domain.repository.DeviceAndSpaceRepository
+import com.aposiamp.smartliving.domain.repository.SpaceRepository
 import com.aposiamp.smartliving.domain.usecase.user.GetCurrentUserUseCase
 
-class GetDevicesSpaceNameUseCase(
-    private val deviceAndSpaceRepository: DeviceAndSpaceRepository,
+class GetSpaceNameUseCase(
+    private val spaceRepository: SpaceRepository,
     private val getCurrentUserUseCase: GetCurrentUserUseCase
 ) {
     suspend fun execute(): SpaceData {
         val currentUser = getCurrentUserUseCase.execute()
         val uid = currentUser?.uid ?: throw Exception("User not logged in")
 
-        val spaceDataDTO = deviceAndSpaceRepository.getDevicesSpaceName(uid)
+        val spaceDataDTO = spaceRepository.getSpaceData(uid)
         val spaceData = SpaceDataMapper.fromDto(spaceDataDTO)
         return spaceData
     }
