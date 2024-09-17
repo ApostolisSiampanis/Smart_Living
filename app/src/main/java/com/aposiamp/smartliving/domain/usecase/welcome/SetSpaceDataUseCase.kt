27282753806932ae1c33a57bad792cb1c4b920e8
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 19:00:33 +0300

    Update SetSpaceDataUseCase

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/SetSpaceDataUseCase.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/SetSpaceDataUseCase.kt
@@ -2,18 +2,18 @@ package com.aposiamp.smartliving.domain.usecase.welcome
 
 import com.aposiamp.smartliving.domain.mapper.SpaceDataMapper
 import com.aposiamp.smartliving.domain.model.SpaceData
-import com.aposiamp.smartliving.domain.repository.DeviceAndSpaceRepository
+import com.aposiamp.smartliving.domain.repository.SpaceRepository
 import com.aposiamp.smartliving.domain.usecase.user.GetCurrentUserUseCase
 
 class SetSpaceDataUseCase(
-    private val deviceAndSpaceRepository: DeviceAndSpaceRepository,
+    private val spaceRepository: SpaceRepository,
     private val getCurrentUserUseCase: GetCurrentUserUseCase
 ) {
     suspend fun execute(spaceData: SpaceData) {
         val currentUser = getCurrentUserUseCase.execute()
         val uid = currentUser?.uid ?: throw Exception("User not logged in")
 
-        deviceAndSpaceRepository.setDevicesSpaceData(
+        spaceRepository.setSpaceData(
             uid,
             SpaceDataMapper.toDto(spaceData)
         )
