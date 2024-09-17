69558631c5903811ec0e44a0e61e7a06590d69c7
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 19:01:03 +0300

    Update PlacesRepositoryImpl, add getLocationFromPlaceId

--- app/src/main/java/com/aposiamp/smartliving/data/repository/PlacesRepositoryImpl.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/repository/PlacesRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.aposiamp.smartliving.data.repository
 
 import com.aposiamp.smartliving.data.model.AutoCompletePredictionData
+import com.aposiamp.smartliving.data.model.PlaceDataDTO
 import com.aposiamp.smartliving.data.source.remote.PlacesDataSource
 import com.aposiamp.smartliving.domain.repository.PlacesRepository
 
@@ -9,5 +10,9 @@ class PlacesRepositoryImpl(
 ) : PlacesRepository {
     override suspend fun getAutocompletePredictions(query: String): List<AutoCompletePredictionData> {
         return placesDataSource.getAutocompletePredictions(query)
+    }
+
+    override suspend fun getLocationFromPlaceId(placeId: String): PlaceDataDTO {
+        return placesDataSource.getLocationFromPlaceId(placeId)
     }
 }
