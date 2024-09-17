9560f8bb35882e8e8abecefc1ca35a9c8d53c9c8
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 19:02:30 +0300

    Update CreateSpaceFormState

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/state/CreateSpaceFormState.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/state/CreateSpaceFormState.kt
@@ -2,5 +2,9 @@ package com.aposiamp.smartliving.presentation.ui.state
 
 data class CreateSpaceFormState(
     val spaceName: String = "",
-    val spaceNameError: String? = null
+    val spaceNameError: String? = null,
+    val spaceAddress: String = "",
+    val spaceAddressError: String? = null,
+    val spaceId: String = "",
+    val spaceIdError: String? = null
 )
