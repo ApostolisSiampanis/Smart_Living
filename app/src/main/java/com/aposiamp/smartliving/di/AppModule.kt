2d0502c7c24ca0673ec4660c3e12714f0df70e33
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 18:43:22 +0300

    Update AppModule, add SpaceRepository,GetLocationFromPlaceIdUseCase,ValidateAddressProximityUseCase,GetSpaceNameUseCase, ValidateSpaceAddress and ValidatePlaceData

--- app/src/main/java/com/aposiamp/smartliving/di/AppModule.kt
+++ app/src/main/java/com/aposiamp/smartliving/di/AppModule.kt
@@ -3,16 +3,17 @@ package com.aposiamp.smartliving.di
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import com.aposiamp.smartliving.domain.repository.AuthRepository
-import com.aposiamp.smartliving.domain.repository.DeviceAndSpaceRepository
+import com.aposiamp.smartliving.domain.repository.SpaceRepository
 import com.aposiamp.smartliving.domain.repository.EnvironmentalSensorRepository
 import com.aposiamp.smartliving.domain.repository.LocationRepository
 import com.aposiamp.smartliving.domain.repository.PlacesRepository
 import com.aposiamp.smartliving.domain.usecase.location.GetLocationDataUseCase
 import com.aposiamp.smartliving.domain.usecase.main.GetBottomNavigationItemsUseCase
-import com.aposiamp.smartliving.domain.usecase.main.GetDevicesSpaceNameUseCase
+import com.aposiamp.smartliving.domain.usecase.main.GetSpaceNameUseCase
 import com.aposiamp.smartliving.domain.usecase.main.GetDropdownMenuItemsUseCase
 import com.aposiamp.smartliving.domain.usecase.main.GetNavigationDrawerItemsUseCase
 import com.aposiamp.smartliving.domain.usecase.places.GetAutoCompleteSuggestionsUseCase
+import com.aposiamp.smartliving.domain.usecase.places.GetLocationFromPlaceIdUseCase
 import com.aposiamp.smartliving.domain.usecase.sensor.GetEnvironmentalDataUseCase
 import com.aposiamp.smartliving.domain.usecase.sensor.SetEnvironmentalDataUseCase
 import com.aposiamp.smartliving.domain.usecase.user.GetCurrentUserUseCase
@@ -21,10 +22,13 @@ import com.aposiamp.smartliving.domain.usecase.user.LogoutUseCase
 import com.aposiamp.smartliving.domain.usecase.user.SignUpUseCase
 import com.aposiamp.smartliving.domain.usecase.welcome.CheckIfSpaceDataExistsUseCase
 import com.aposiamp.smartliving.domain.usecase.welcome.SetSpaceDataUseCase
+import com.aposiamp.smartliving.domain.usecase.welcome.ValidateAddressProximityUseCase
 import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidateEmail
 import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidateFirstName
 import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidateLastName
 import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidatePassword
+import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidatePlaceData
+import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidateSpaceAddress
 import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidateSpaceName
 import com.aposiamp.smartliving.domain.usecase.welcome.validateregex.ValidateTerms
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -38,7 +42,7 @@ interface AppModule {
     val authRepository: AuthRepository
     val environmentalSensorRepository: EnvironmentalSensorRepository
     val locationRepository: LocationRepository
-    val deviceAndSpaceRepository: DeviceAndSpaceRepository
+    val spaceRepository: SpaceRepository
     val placesRepository: PlacesRepository
 
     // Firebase
@@ -72,13 +76,15 @@ interface AppModule {
 
     // Location UseCases
     val getLocationDataUseCase: GetLocationDataUseCase
+    val getLocationFromPlaceIdUseCase: GetLocationFromPlaceIdUseCase
 
     // Places UseCases
     val getAutoCompleteSuggestionsUseCase: GetAutoCompleteSuggestionsUseCase
+    val validateAddressProximityUseCase: ValidateAddressProximityUseCase
 
     // Space UseCases
     val setSpaceDataUseCase: SetSpaceDataUseCase
-    val getDevicesSpaceNameUseCase: GetDevicesSpaceNameUseCase
+    val getSpaceNameUseCase: GetSpaceNameUseCase
     val checkIfSpaceDataExistsUseCase: CheckIfSpaceDataExistsUseCase
 
     // Profile UseCases
@@ -95,4 +101,6 @@ interface AppModule {
     val validateTerms: ValidateTerms
     // For CreateANewSpace screen
     val validateSpaceName: ValidateSpaceName
+    val validateSpaceAddress: ValidateSpaceAddress
+    val validatePlaceData: ValidatePlaceData
 }
