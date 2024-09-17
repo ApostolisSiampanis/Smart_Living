71b9d8d2a8cbe21d3c7452abe55c6cfbb7849644
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-17 18:33:55 +0300

    Delete DeviceAndSpaceRepository.kt

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceAndSpaceRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.aposiamp.smartliving.domain.repository
-
-import com.aposiamp.smartliving.data.model.SpaceDataDTO
-
-interface DeviceAndSpaceRepository {
-    suspend fun setDevicesSpaceData(userId: String, spaceDataDTO: SpaceDataDTO)
-    suspend fun getDevicesSpaceName(userId: String): SpaceDataDTO
-    suspend fun checkIfSpaceDataExists(userId: String): Boolean
-}
