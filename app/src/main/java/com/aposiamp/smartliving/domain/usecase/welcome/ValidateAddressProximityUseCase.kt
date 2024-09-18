ff5315f6182920cffca31dd252a6421f886e57c5
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 11:28:44 +0300

    Remove private fun calculateDistance and move it to DistanceUtils

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/ValidateAddressProximityUseCase.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/welcome/ValidateAddressProximityUseCase.kt
@@ -3,10 +3,7 @@ package com.aposiamp.smartliving.domain.usecase.welcome
 import com.aposiamp.smartliving.domain.model.PlaceData
 import com.aposiamp.smartliving.domain.usecase.location.GetLocationDataUseCase
 import com.aposiamp.smartliving.domain.usecase.places.GetLocationFromPlaceIdUseCase
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
+import com.aposiamp.smartliving.domain.utils.DistanceUtils
 
 class ValidateAddressProximityUseCase(
     private val getLocationDataUseCase: GetLocationDataUseCase,
@@ -16,28 +13,12 @@ class ValidateAddressProximityUseCase(
         val currentLocation = getLocationDataUseCase.execute()
         val addressLocation = getLocationFromPlaceIdUseCase.execute(placeId)
 
-        val distance = calculateDistance (
+        val distance = DistanceUtils.calculateDistance (
             currentLocation.latitude ?: 0.0,
             currentLocation.longitude ?: 0.0,
             addressLocation.location?.latitude ?: 0.0,
             addressLocation.location?.longitude ?: 0.0
         )
         return if (distance <= 1000) addressLocation else null
-    }
-
-    private fun calculateDistance(
-        lat1: Double,
-        lon1: Double,
-        lat2: Double,
-        lon2: Double
-    ): Double {
-        val earthRadius = 6371000.0 // meters
-        val dLat = Math.toRadians(lat2 - lat1)
-        val dLon = Math.toRadians(lon2 - lon1)
-        val a = sin(dLat / 2) * sin(dLat / 2) +
-                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
-                sin(dLon / 2) * sin(dLon / 2)
-        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        return earthRadius * c
     }
 }
