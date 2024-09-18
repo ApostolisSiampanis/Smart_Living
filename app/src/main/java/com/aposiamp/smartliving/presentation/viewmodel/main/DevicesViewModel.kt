756a55b64b0d9371120f58a1385a029e832d1737
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 11:44:30 +0300

    Remove getDevicesSpaceNameUseCase from DevicesViewModel

--- app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/DevicesViewModel.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/DevicesViewModel.kt
@@ -2,25 +2,13 @@ package com.aposiamp.smartliving.presentation.viewmodel.main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.aposiamp.smartliving.domain.usecase.main.GetDevicesSpaceNameUseCase
+import com.aposiamp.smartliving.domain.model.SpaceData
+import com.aposiamp.smartliving.domain.usecase.main.GetSpaceUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class DevicesViewModel(
-    private val getDevicesSpaceNameUseCase: GetDevicesSpaceNameUseCase
 ) : ViewModel() {
-    private val _spaceName = MutableStateFlow("")
-    val spaceName: StateFlow<String> = _spaceName
 
-    init {
-        fetchSpaceName()
-    }
-
-    private fun fetchSpaceName() {
-        viewModelScope.launch {
-            val spaceData = getDevicesSpaceNameUseCase.execute()
-            _spaceName.value = spaceData.spaceName.toString()
-        }
-    }
 }
