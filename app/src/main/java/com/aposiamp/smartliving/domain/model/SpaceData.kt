8f8b89b5881109eb256f0222dd84af74cd044c1c
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 14:23:14 +0300

    Update SpaceData, add placeName field

--- app/src/main/java/com/aposiamp/smartliving/domain/model/SpaceData.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/model/SpaceData.kt
@@ -2,6 +2,7 @@ package com.aposiamp.smartliving.domain.model
 
 data class SpaceData(
     val placeId: String?,
+    val placeName: String?,
     val spaceName: String?,
     val fullAddress: String?,
     val location: LocationData?
