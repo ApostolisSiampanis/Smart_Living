6dda962716afb883c60d713730dee5ec1d57a0d3
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 11:28:11 +0300

    Create DistanceUtils, calculateDistance method

---
+++ app/src/main/java/com/aposiamp/smartliving/domain/utils/DistanceUtils.kt
@@ -0,0 +1,24 @@
+package com.aposiamp.smartliving.domain.utils
+
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+object DistanceUtils {
+    fun calculateDistance(
+        lat1: Double,
+        lon1: Double,
+        lat2: Double,
+        lon2: Double
+    ): Double {
+        val earthRadius = 6371000.0 // meters
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val a = sin(dLat / 2) * sin(dLat / 2) +
+                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+                sin(dLon / 2) * sin(dLon / 2)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return earthRadius * c
+    }
+}
