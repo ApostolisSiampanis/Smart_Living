8540b42b6ed032ae4c47d0ec01de4e366df5c8ce
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 11:05:30 +0300

    Create SpaceDataUiModel

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/model/SpaceDataUiModel.kt
@@ -0,0 +1,8 @@
+package com.aposiamp.smartliving.presentation.model
+
+data class SpaceDataUiModel(
+    val placeId: String?,
+    val spaceName: String?,
+    val fullAddress: String?,
+    val location: LocationDataUiModel?
+)
