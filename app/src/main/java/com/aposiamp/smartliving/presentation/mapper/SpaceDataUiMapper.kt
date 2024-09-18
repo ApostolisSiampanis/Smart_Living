71696a9803e1b9a7699dbea66b85e257c0ed4cb4
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 11:05:36 +0300

    Create SpaceDataUiMapper

---
+++ app/src/main/java/com/aposiamp/smartliving/presentation/mapper/SpaceDataUiMapper.kt
@@ -0,0 +1,15 @@
+package com.aposiamp.smartliving.presentation.mapper
+
+import com.aposiamp.smartliving.domain.model.SpaceData
+import com.aposiamp.smartliving.presentation.model.SpaceDataUiModel
+
+object SpaceDataUiMapper {
+    fun fromDomain(domain: SpaceData): SpaceDataUiModel {
+        return SpaceDataUiModel(
+            placeId = domain.placeId,
+            spaceName = domain.spaceName,
+            fullAddress = domain.fullAddress,
+            location = domain.location?.let { LocationDataUiMapper.fromDomain(it) }
+        )
+    }
+}
