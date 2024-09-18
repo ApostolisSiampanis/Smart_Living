f2ba4f5f983ece493dfc88d7c001a014d15ea251
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 11:36:10 +0300

    Update setEnvironmentalData into FirestoreDataSource to also save the placeId

--- app/src/main/java/com/aposiamp/smartliving/data/source/remote/FirestoreDataSource.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/source/remote/FirestoreDataSource.kt
@@ -11,11 +11,11 @@ class FirestoreDataSource(private val db: FirebaseFirestore) {
         db.collection("users").document(uid).set(user.toMap()).await()
     }
 
-    suspend fun setEnvironmentalData(uid: String, environmentalDataDTO: EnvironmentalDataDTO) {
+    suspend fun setEnvironmentalData(uid: String, placeId: String, environmentalDataDTO: EnvironmentalDataDTO) {
         db.collection("environmental_data")
             .document(uid)
-            .collection(environmentalDataDTO.timestamp.toString())
-            .document("data")
+            .collection(placeId)
+            .document(environmentalDataDTO.timestamp.toString())
             .set(mapOf(
                 "temperature" to environmentalDataDTO.temperature,
                 "humidity" to environmentalDataDTO.humidity
