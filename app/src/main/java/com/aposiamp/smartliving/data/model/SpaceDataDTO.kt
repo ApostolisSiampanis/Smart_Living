11edb86a23e5d19798fa7dba98500029d90a3b1a
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-18 14:23:25 +0300

    Update SpaceDataDTO, add placeName field

--- app/src/main/java/com/aposiamp/smartliving/data/model/SpaceDataDTO.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/model/SpaceDataDTO.kt
@@ -4,6 +4,7 @@ import com.google.firebase.database.PropertyName
 
 data class SpaceDataDTO(
     @get:PropertyName("placeId") @set:PropertyName("placeId") var placeId: String? = null,
+    @get:PropertyName("place_name") @set:PropertyName("place_name") var placeName: String? = null,
     @get:PropertyName("space_name") @set:PropertyName("space_name") var spaceName: String? = null,
     @get:PropertyName("full_address") @set:PropertyName("full_address") var fullAddress: String? = null,
     @get:PropertyName("location") @set:PropertyName("location") var location: LocationDataDTO? = null
