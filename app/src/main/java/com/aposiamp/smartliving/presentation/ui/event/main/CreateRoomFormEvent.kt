b4f918bf2c6011701952b34bd39919179a1715c0
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-19 15:25:09 +0300

    Update CreateRoomFormEvent

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/event/main/CreateRoomFormEvent.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/event/main/CreateRoomFormEvent.kt
@@ -2,5 +2,5 @@ package com.aposiamp.smartliving.presentation.ui.event.main
 
 sealed class CreateRoomFormEvent {
     data class RoomNameChanged(val roomName: String) : CreateRoomFormEvent()
-    data object Submit : CreateRoomFormEvent()
+    data class Submit(val placeId: String) : CreateRoomFormEvent()
 }
