c7f753819e98f7a2f2af9795553a3922ca6590be
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-19 15:23:59 +0300

    Create RoomRepositoryImpl

---
+++ app/src/main/java/com/aposiamp/smartliving/data/repository/RoomRepositoryImpl.kt
@@ -0,0 +1,13 @@
+package com.aposiamp.smartliving.data.repository
+
+import com.aposiamp.smartliving.data.model.RoomDataDTO
+import com.aposiamp.smartliving.data.source.remote.FirebaseDataSource
+import com.aposiamp.smartliving.domain.repository.RoomRepository
+
+class RoomRepositoryImpl(
+    private val firebaseDataSource: FirebaseDataSource
+) : RoomRepository {
+    override suspend fun setRoomData(userId: String, spaceId: String, roomDataDTO: RoomDataDTO) {
+        firebaseDataSource.setRoomData(userId, spaceId, roomDataDTO)
+    }
+}
