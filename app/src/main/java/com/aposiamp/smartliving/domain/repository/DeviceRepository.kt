236ef319446192de6beee5d9bd7688869928ad0e
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-21 20:14:18 +0300

    Update DeviceRepository, add setDeviceData

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
@@ -1,7 +1,9 @@
 package com.aposiamp.smartliving.domain.repository
 
+import com.aposiamp.smartliving.data.model.DeviceDataDTO
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
 
 interface DeviceRepository {
     suspend fun checkIfDeviceExists(deviceIdAndTypeDTO: DeviceIdAndTypeDTO): Boolean
+    suspend fun setDeviceData(userId: String, spaceId: String, roomId: String, deviceDataDTO: DeviceDataDTO)
 }
