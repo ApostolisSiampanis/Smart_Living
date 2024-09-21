9ddd5036b259d0f78d5eb7df6f6dc46feb6008a8
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-21 15:48:30 +0300

    Update RoomRepository, add getRoomList

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/RoomRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/RoomRepository.kt
@@ -4,4 +4,5 @@ import com.aposiamp.smartliving.data.model.RoomDataDTO
 
 interface RoomRepository {
     suspend fun setRoomData(userId: String, spaceId: String, roomDataDTO: RoomDataDTO)
+    suspend fun getRoomList(userId: String, spaceId: String): List<RoomDataDTO>?
 }
