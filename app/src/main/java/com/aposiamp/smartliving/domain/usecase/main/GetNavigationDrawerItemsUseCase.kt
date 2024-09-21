9d2809a2abc69e9161538f8e3ec18fcbfae35272
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-21 22:24:00 +0300

    Update GetNavigationDrawerItemsUseCase

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/main/GetNavigationDrawerItemsUseCase.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/main/GetNavigationDrawerItemsUseCase.kt
@@ -3,8 +3,9 @@ package com.aposiamp.smartliving.domain.usecase.main
 import com.aposiamp.smartliving.domain.model.NavigationItemData
 
 class GetNavigationDrawerItemsUseCase {
-    fun execute(settingsTitle: String, aboutTitle: String, logoutTitle: String): List<NavigationItemData> {
+    fun execute(homeTitle: String, settingsTitle: String, aboutTitle: String, logoutTitle: String): List<NavigationItemData> {
         return listOf(
+            NavigationItemData(title = homeTitle),
             NavigationItemData(title = settingsTitle),
             NavigationItemData(title = aboutTitle),
             NavigationItemData(title = logoutTitle)
