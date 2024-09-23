36a8cf3c38e028fb7044520f8e095ec6d23fbe32
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-23 20:57:48 +0300

    Update TextComponents.kt, update NameFieldComponent

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/TextComponents.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/TextComponents.kt
@@ -1,17 +1,14 @@
 package com.aposiamp.smartliving.presentation.ui.component
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -245,8 +242,6 @@ fun NameFieldComponent(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .wrapContentWidth(Alignment.CenterHorizontally),
-        horizontalArrangement = Arrangement.Center
     ) {
         GeneralBoldText(
             value = firstName ?: "",
