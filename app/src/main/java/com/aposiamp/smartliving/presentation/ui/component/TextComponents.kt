4ff20df469af8628d40ac114df191237065c1be2
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-23 19:27:43 +0300

    Update TextComponents, add NameFieldComponent

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/TextComponents.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/TextComponents.kt
@@ -1,11 +1,17 @@
 package com.aposiamp.smartliving.presentation.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -17,7 +23,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aposiamp.smartliving.R
+import com.aposiamp.smartliving.presentation.ui.theme.BlueGreen
 import com.aposiamp.smartliving.presentation.ui.theme.GrayColor
+import com.aposiamp.smartliving.presentation.ui.theme.PrussianBlue
 
 @Composable
 fun HeadingTextComponent(value: String) {
@@ -227,4 +235,29 @@ fun IndicatorBoldTextComponent(
         fontFamily = FontFamily(Font(R.font.carlito_bold)),
         modifier = modifier
     )
+}
+
+@Composable
+fun NameFieldComponent(
+    firstName: String?,
+    lastName: String?
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        GeneralBoldText(
+            value = firstName ?: "",
+            fontSize = 22,
+            color = PrussianBlue
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        GeneralBoldText(
+            value = lastName ?: "",
+            fontSize = 22,
+            color = BlueGreen
+        )
+    }
 }
