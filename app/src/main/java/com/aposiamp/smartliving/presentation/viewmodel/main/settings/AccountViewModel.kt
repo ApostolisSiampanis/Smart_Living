5a86b85f1153ec1210a2370a7e70c060436d88b2
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-24 09:33:26 +0300

    Update AccountViewModel

--- app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/settings/AccountViewModel.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/settings/AccountViewModel.kt
@@ -1,25 +1,9 @@
 package com.aposiamp.smartliving.presentation.viewmodel.main.settings
 
 import androidx.lifecycle.ViewModel
-import com.aposiamp.smartliving.domain.usecase.user.GetAccountProfileDetailsUseCase
-import com.aposiamp.smartliving.presentation.mapper.UserAccountUiMapper
-import com.aposiamp.smartliving.presentation.model.AccountProfileUiModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
 class AccountViewModel(
-    private val getAccountProfileDetailsUseCase: GetAccountProfileDetailsUseCase
+
 ): ViewModel() {
-    private val _accountDetailsState = MutableStateFlow<AccountProfileUiModel?>(null)
-    val accountDetailsState: StateFlow<AccountProfileUiModel?> get() = _accountDetailsState
 
-    private val _isLoading = MutableStateFlow(false)
-    val isLoading: StateFlow<Boolean> get() = _isLoading
-
-    suspend fun fetchAccountDetails() {
-        _isLoading.value = true
-        val userAccountData = getAccountProfileDetailsUseCase.execute()
-        _accountDetailsState.value = UserAccountUiMapper.fromDomain(userAccountData)
-        _isLoading.value = false
-    }
 }
