99a8c9e20d06865bb1c5734b7474ec79add09d6d
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 01:40:11 +0300

    Update AirConditionRepository, add updateTemperature

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/AirConditionRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/AirConditionRepository.kt
@@ -2,8 +2,10 @@ package com.aposiamp.smartliving.domain.repository
 
 import com.aposiamp.smartliving.data.model.AirDirectionDTO
 import com.aposiamp.smartliving.data.model.FanSpeedDTO
+import com.aposiamp.smartliving.data.model.TemperatureDTO
 
 interface AirConditionRepository {
     suspend fun updateAirDirection(airConditionId: String, airDirectionDTO: AirDirectionDTO): Boolean
     suspend fun updateFanSpeed(airConditionId: String, fanSpeedDTO: FanSpeedDTO): Boolean
+    suspend fun updateTemperature(airConditionId: String, temperatureDTO: TemperatureDTO): Boolean
 }
