910fa2c25faa481bf11ceabe16513cd24d2aa9b4
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 13:22:35 +0300

    Update ThermostatStatusDTO

--- app/src/main/java/com/aposiamp/smartliving/data/model/ThermostatStatusDTO.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/model/ThermostatStatusDTO.kt
@@ -2,11 +2,8 @@ package com.aposiamp.smartliving.data.model
 
 import com.aposiamp.smartliving.domain.model.DeviceMode
 import com.aposiamp.smartliving.domain.model.DeviceState
-import com.aposiamp.smartliving.domain.model.DeviceType
 
 data class ThermostatStatusDTO(
-    val id: String,
-    val type: DeviceType,
     val state: DeviceState,
     val mode: DeviceMode,
     val temperature: Int
