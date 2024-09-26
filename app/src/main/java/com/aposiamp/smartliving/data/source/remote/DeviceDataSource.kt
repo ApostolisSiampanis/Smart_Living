32c7ec1fb6dde05852767871479c30b9601bc756
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 13:12:33 +0300

    Update DeviceDataSource, add getThermostatStatus

--- app/src/main/java/com/aposiamp/smartliving/data/source/remote/DeviceDataSource.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/source/remote/DeviceDataSource.kt
@@ -1,6 +1,7 @@
 package com.aposiamp.smartliving.data.source.remote
 
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
+import com.aposiamp.smartliving.data.model.ThermostatStatusDTO
 
 class DeviceDataSource(
     private val apiService: DeviceApiService
@@ -11,6 +12,19 @@ class DeviceDataSource(
             response.isSuccessful
         } catch (e: Exception) {
             false
+        }
+    }
+
+    suspend fun getThermostatStatus(deviceId: String): ThermostatStatusDTO? {
+        return try {
+            val response = apiService.getThermostatStatus(deviceId)
+            if (response.isSuccessful) {
+                response.body()
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            null
         }
     }
 }
