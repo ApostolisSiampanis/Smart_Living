748dc5b170eeb92eaa96c8b672e7a9d3d3de0cf3
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 01:30:19 +0300

    Update AirConditionDataSource, add updateFanSpeed

--- app/src/main/java/com/aposiamp/smartliving/data/source/remote/AirConditionDataSource.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/source/remote/AirConditionDataSource.kt
@@ -1,13 +1,23 @@
 package com.aposiamp.smartliving.data.source.remote
 
 import com.aposiamp.smartliving.data.model.AirDirectionDTO
+import com.aposiamp.smartliving.data.model.FanSpeedDTO
 
 class AirConditionDataSource(
     private val airConditionApiService: AirConditionApiService
 ) {
-    suspend fun updateAirDirection(airConditionId: String, airDirection: AirDirectionDTO): Boolean {
+    suspend fun updateAirDirection(airConditionId: String, airDirectionDTO: AirDirectionDTO): Boolean {
         return try {
-            val response = airConditionApiService.updateAirDirection(airConditionId, airDirection)
+            val response = airConditionApiService.updateAirDirection(airConditionId, airDirectionDTO)
+            response.isSuccessful
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    suspend fun updateFanSpeed(airConditionId: String, fanSpeedDTO: FanSpeedDTO): Boolean {
+        return try {
+            val response = airConditionApiService.updateFanSpeed(airConditionId, fanSpeedDTO)
             response.isSuccessful
         } catch (e: Exception) {
             false
