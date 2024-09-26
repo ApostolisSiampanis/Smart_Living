aff2aadc7d23a967c17623c641f17b713c4ca6b4
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 00:34:22 +0300

    Update DehumidifierRepositoryImpl, add updateFanSpeed

--- app/src/main/java/com/aposiamp/smartliving/data/repository/DehumidifierRepositoryImpl.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/repository/DehumidifierRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.aposiamp.smartliving.data.repository
 
+import com.aposiamp.smartliving.data.model.FanSpeedDTO
 import com.aposiamp.smartliving.data.model.HumidityLevelDTO
 import com.aposiamp.smartliving.data.source.remote.DehumidifierDataSource
 import com.aposiamp.smartliving.domain.repository.DehumidifierRepository
@@ -9,5 +10,9 @@ class DehumidifierRepositoryImpl(
 ) : DehumidifierRepository {
     override suspend fun updateHumidityLevel(dehumidifierId: String, humidityLevelDTO: HumidityLevelDTO): Boolean {
         return dehumidifierDataSource.updateHumidityLevel(dehumidifierId, humidityLevelDTO)
+    }
+
+    override suspend fun updateFanSpeed(dehumidifierId: String, fanSpeedDTO: FanSpeedDTO): Boolean {
+        return dehumidifierDataSource.updateFanSpeed(dehumidifierId, fanSpeedDTO)
     }
 }
