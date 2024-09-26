c3b797e9f813a4e4ee5962df62fb98bac9fb7934
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 21:34:35 +0300

    Update DeviceDataSource, add updateDeviceMode

--- app/src/main/java/com/aposiamp/smartliving/data/source/remote/DeviceDataSource.kt
+++ app/src/main/java/com/aposiamp/smartliving/data/source/remote/DeviceDataSource.kt
@@ -3,6 +3,7 @@ package com.aposiamp.smartliving.data.source.remote
 import com.aposiamp.smartliving.data.model.AirConditionStatusDTO
 import com.aposiamp.smartliving.data.model.DehumidifierStatusDTO
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
+import com.aposiamp.smartliving.data.model.DeviceModeDTO
 import com.aposiamp.smartliving.data.model.DeviceStateDTO
 import com.aposiamp.smartliving.data.model.ThermostatStatusDTO
 
@@ -60,6 +61,15 @@ class DeviceDataSource(
     suspend fun updateDeviceState(deviceId: String, deviceStateDTO: DeviceStateDTO): Boolean {
         return try {
             val response = apiService.updateDeviceState(deviceId, deviceStateDTO)
+            response.isSuccessful
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    suspend fun updateDeviceMode(deviceId: String, deviceModeDTO: DeviceModeDTO): Boolean {
+        return try {
+            val response = apiService.updateDeviceMode(deviceId, deviceModeDTO)
             response.isSuccessful
         } catch (e: Exception) {
             false
