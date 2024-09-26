d46b88c3ab9010394d49534f45c2fd5c2127b8ad
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 13:12:42 +0300

    Update DeviceRepository, add getThermostatStatus

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
@@ -2,9 +2,11 @@ package com.aposiamp.smartliving.domain.repository
 
 import com.aposiamp.smartliving.data.model.DeviceDataDTO
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
+import com.aposiamp.smartliving.data.model.ThermostatStatusDTO
 
 interface DeviceRepository {
     suspend fun checkIfDeviceExists(deviceIdAndTypeDTO: DeviceIdAndTypeDTO): Boolean
     suspend fun setDeviceData(userId: String, spaceId: String, roomId: String, deviceDataDTO: DeviceDataDTO)
     suspend fun getDeviceList(userId: String, spaceId: String, roomId: String): List<DeviceDataDTO>?
+    suspend fun getThermostatStatus(deviceId: String): ThermostatStatusDTO?
 }
