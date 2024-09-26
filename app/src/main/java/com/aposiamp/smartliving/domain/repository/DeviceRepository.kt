99e58a387a77a52a9b6be63e28f8e74618f0305f
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 21:34:56 +0300

    Update DeviceRepository, add updateDeviceMode

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
@@ -4,6 +4,7 @@ import com.aposiamp.smartliving.data.model.AirConditionStatusDTO
 import com.aposiamp.smartliving.data.model.DehumidifierStatusDTO
 import com.aposiamp.smartliving.data.model.DeviceDataDTO
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
+import com.aposiamp.smartliving.data.model.DeviceModeDTO
 import com.aposiamp.smartliving.data.model.DeviceStateDTO
 import com.aposiamp.smartliving.data.model.ThermostatStatusDTO
 
@@ -15,4 +16,5 @@ interface DeviceRepository {
     suspend fun getAirConditionStatus(deviceId: String): AirConditionStatusDTO?
     suspend fun getDehumidifierStatus(deviceId: String): DehumidifierStatusDTO?
     suspend fun updateDeviceState(deviceId: String, deviceStateDTO: DeviceStateDTO): Boolean
+    suspend fun updateDeviceMode(deviceId: String, deviceModeDTO: DeviceModeDTO): Boolean
 }
