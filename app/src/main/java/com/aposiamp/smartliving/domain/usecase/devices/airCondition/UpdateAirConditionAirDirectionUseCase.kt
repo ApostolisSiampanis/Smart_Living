6c27e22a8ca693f7c020b2363cc6cbd14f1ce884
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 01:29:26 +0300

    Rename UpdateAirDirectionUseCase to UpdateAirConditionAirDirectionUseCase

--- app/src/main/java/com/aposiamp/smartliving/domain/usecase/devices/airCondition/UpdateAirConditionAirDirectionUseCase.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/usecase/devices/airCondition/UpdateAirConditionAirDirectionUseCase.kt
@@ -3,7 +3,7 @@ package com.aposiamp.smartliving.domain.usecase.devices.airCondition
 import com.aposiamp.smartliving.data.model.AirDirectionDTO
 import com.aposiamp.smartliving.domain.repository.AirConditionRepository
 
-class UpdateAirDirectionUseCase(
+class UpdateAirConditionAirDirectionUseCase(
     private val airConditionRepository: AirConditionRepository
 ) {
     suspend fun execute(airConditionId: String, airDirection: Int): Boolean {
