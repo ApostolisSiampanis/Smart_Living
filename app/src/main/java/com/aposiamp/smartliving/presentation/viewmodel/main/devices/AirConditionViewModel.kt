abeadac326264321ea012cadeef1429734913e7b
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 14:41:16 +0300

    Update AirConditionViewModel

--- app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/devices/AirConditionViewModel.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/devices/AirConditionViewModel.kt
@@ -1,11 +1,14 @@
 package com.aposiamp.smartliving.presentation.viewmodel.main.devices
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.aposiamp.smartliving.R
+import com.aposiamp.smartliving.domain.model.AirConditionStatusData
 import com.aposiamp.smartliving.domain.model.DeviceMode
 import com.aposiamp.smartliving.domain.model.DeviceModeItem
 import com.aposiamp.smartliving.domain.model.DeviceState
 import com.aposiamp.smartliving.domain.model.DeviceStateItem
+import com.aposiamp.smartliving.domain.usecase.devices.airCondition.GetAirConditionStatusUseCase
 import com.aposiamp.smartliving.presentation.model.DeviceModeUiItem
 import com.aposiamp.smartliving.presentation.model.DeviceStateUiItem
 import com.aposiamp.smartliving.presentation.ui.theme.Blue
@@ -14,8 +17,13 @@ import com.aposiamp.smartliving.presentation.ui.theme.DryBlue
 import com.aposiamp.smartliving.presentation.ui.theme.LightOrange
 import com.aposiamp.smartliving.presentation.ui.theme.Orange
 import com.aposiamp.smartliving.presentation.ui.theme.RedOrange
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class AirConditionViewModel : ViewModel() {
+class AirConditionViewModel(
+    private val getAirConditionStatusUseCase: GetAirConditionStatusUseCase
+) : ViewModel() {
     private val deviceStates = listOf(
         DeviceStateItem(DeviceState.OFF),
         DeviceStateItem(DeviceState.ON)
@@ -74,6 +82,15 @@ class AirConditionViewModel : ViewModel() {
                 secondaryColor = DryBlue
             )
             else -> null
+        }
+    }
+
+    private val _deviceStatus = MutableStateFlow<AirConditionStatusData?>(null)
+    val deviceStatus: StateFlow<AirConditionStatusData?> = _deviceStatus
+
+    fun fetchDeviceStatus(deviceId: String) {
+        viewModelScope.launch {
+            _deviceStatus.value = getAirConditionStatusUseCase.execute(deviceId)
         }
     }
 }
