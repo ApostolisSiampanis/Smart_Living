f7b6e0fd0d1d9014d11649f51d07aeee56364f31
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 16:18:48 +0300

    Update DehumidifierScreen to use deviceStatus

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/screens/devices/DehumidifierScreen.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/activity/main/screens/devices/DehumidifierScreen.kt
@@ -10,16 +10,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.aposiamp.smartliving.presentation.ui.activity.LoadingScreen
 import com.aposiamp.smartliving.presentation.ui.component.BackAppTopBar
 import com.aposiamp.smartliving.presentation.ui.component.DehumidifierIndicator
 import com.aposiamp.smartliving.presentation.ui.component.DeviceIndicatorCard
@@ -33,92 +32,107 @@ import com.aposiamp.smartliving.presentation.viewmodel.main.MainSharedViewModel
 @Composable
 fun DehumidifierScreen(
     navController: NavController,
+    viewModel: DehumidifierViewModel,
     sharedViewModel: MainSharedViewModel
 ) {
-    val viewModel = DehumidifierViewModel()
     val uiDeviceStates = viewModel.uiDeviceStates
     val uiDeviceModes = viewModel.uiDeviceModes
-    var selectedState by remember { mutableStateOf(uiDeviceStates[0]) }
-    var selectedMode by remember { mutableStateOf(uiDeviceModes[0]) }
 
     val selectedDevice by sharedViewModel.selectedDevice.collectAsState()
+    val deviceStatus by viewModel.deviceStatus.collectAsState()
 
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize(),
-        topBar = {
-            BackAppTopBar(
-                title = selectedDevice?.deviceName ?: "",
-                color = MaterialTheme.colorScheme.primaryContainer,
-                onBackClick = {
-                    navController.navigateUp()
-                }
-            )
-        },
-        content = { padding ->
-            Surface(
-                color = Color.White,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding)
-            ) {
-                LazyColumn(
+    LaunchedEffect(selectedDevice) {
+        selectedDevice?.let {
+            viewModel.fetchDeviceStatus(it.deviceId!!)
+        }
+    }
+
+    if (deviceStatus == null) {
+        LoadingScreen()
+    } else {
+        val selectedState = uiDeviceStates.first { it.state == deviceStatus!!.state }
+        val selectedMode = uiDeviceModes.first { it.mode == deviceStatus!!.mode }
+
+        Scaffold(
+            modifier = Modifier
+                .fillMaxSize(),
+            topBar = {
+                BackAppTopBar(
+                    title = selectedDevice?.deviceName ?: "",
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    onBackClick = {
+                        navController.navigateUp()
+                    }
+                )
+            },
+            content = { padding ->
+                Surface(
+                    color = Color.White,
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(top = 18.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        .padding(padding)
                 ) {
-                    item {
-                        DeviceIndicatorCard(
-                            modifier = Modifier
-                                .fillMaxWidth(0.8f)
-                        ) {
-                            DehumidifierIndicator(
-                                minValue = 40,
-                                maxValue = 95,
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 18.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        item {
+                            DeviceIndicatorCard(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.8f)
+                            ) {
+                                DehumidifierIndicator(
+                                    minValue = 40,
+                                    maxValue = 95,
+                                    selectedState = selectedState,
+                                    selectedMode = selectedMode,
+                                    initialValue = deviceStatus!!.humidityLevel,
+                                    onSetValue = { value ->
+                                        // TODO: Update device set humidity level
+                                    }
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(16.dp))
+                            IndoorEnvironmentalDataCard(
+                                indoorTemperature = sharedViewModel.environmentalData.value?.temperature,
+                                indoorHumidity = sharedViewModel.environmentalData.value?.humidity
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            DeviceOnOffButton(
+                                initialState = selectedState,
+                                color = selectedMode.secondaryColor,
+                                onButtonClicked = { state ->
+                                    //selectedState = uiDeviceStates.first { it.state == state }
+                                    //TODO: Update device status state
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            FanSpeedControl(
+                                initialSpeed = deviceStatus!!.fanSpeed,
+                                maxSpeed = 5,
+                                color = selectedMode.secondaryColor,
+                                isDehumidifier = true,
                                 selectedState = selectedState,
                                 selectedMode = selectedMode,
-                                onSetValue = { value ->
-
+                                onSpeedChange = { speed ->
+                                    //TODO: Update device status fan speed
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            DeviceModeButtonsRowComponent(
+                                modes = uiDeviceModes,
+                                selectedMode = selectedMode,
+                                onButtonClicked = { mode ->
+                                    //selectedMode = mode
+                                    //TODO: Update device status mode
                                 }
                             )
                         }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        IndoorEnvironmentalDataCard(
-                            indoorTemperature = sharedViewModel.environmentalData.value?.temperature,
-                            indoorHumidity = sharedViewModel.environmentalData.value?.humidity
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        DeviceOnOffButton(
-                            initialState = selectedState,
-                            color = selectedMode.secondaryColor,
-                            onButtonClicked = { state ->
-                                selectedState = uiDeviceStates.first { it.state == state }
-                            }
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        FanSpeedControl(
-                            initialSpeed = 1,
-                            maxSpeed = 5,
-                            color = selectedMode.secondaryColor,
-                            isDehumidifier = true,
-                            selectedState = selectedState,
-                            selectedMode = selectedMode,
-                            onSpeedChange = { speed ->
-
-                            }
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        DeviceModeButtonsRowComponent(
-                            modes = uiDeviceModes,
-                            selectedMode = selectedMode,
-                            onButtonClicked = { mode ->
-                                selectedMode = mode
-                            }
-                        )
                     }
                 }
             }
-        }
-    )
+        )
+    }
 }
