2047c2077a897ee35d617dfc001f7eb62e11c663
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-26 14:40:01 +0300

    Create DeviceRepository

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
@@ -1,5 +1,6 @@
 package com.aposiamp.smartliving.domain.repository
 
+import com.aposiamp.smartliving.data.model.AirConditionStatusDTO
 import com.aposiamp.smartliving.data.model.DeviceDataDTO
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
 import com.aposiamp.smartliving.data.model.ThermostatStatusDTO
@@ -9,4 +10,5 @@ interface DeviceRepository {
     suspend fun setDeviceData(userId: String, spaceId: String, roomId: String, deviceDataDTO: DeviceDataDTO)
     suspend fun getDeviceList(userId: String, spaceId: String, roomId: String): List<DeviceDataDTO>?
     suspend fun getThermostatStatus(deviceId: String): ThermostatStatusDTO?
+    suspend fun getAirConditionStatus(deviceId: String): AirConditionStatusDTO?
 }
