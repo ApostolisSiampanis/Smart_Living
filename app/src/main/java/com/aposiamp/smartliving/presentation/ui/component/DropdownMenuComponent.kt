c306f1d504cfa3c088f81a4697af78b0b7fc9230
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-27 16:26:38 +0300

    Create PeriodDropdownMenu into DropdownMenuComponent

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/DropdownMenuComponent.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/DropdownMenuComponent.kt
@@ -1,9 +1,21 @@
 package com.aposiamp.smartliving.presentation.ui.component
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.aposiamp.smartliving.presentation.model.DropdownMenuItemUiModel
+import com.aposiamp.smartliving.presentation.model.PeriodItemUiModel
 
 @Composable
 fun DropdownMenuComponent(
@@ -23,6 +35,47 @@ fun DropdownMenuComponent(
                     onDismissRequest()
                 }
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PeriodDropdownMenu(
+    items: List<PeriodItemUiModel>,
+    selectedItem: PeriodItemUiModel?,
+    onItemSelected: (PeriodItemUiModel) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedPeriod by remember { mutableStateOf(selectedItem ?: items.firstOrNull()) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }
+    ) {
+        OutlinedTextField(
+            value = selectedPeriod?.textResId?.let { stringResource(id = it) } ?: "",
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .menuAnchor()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            items.forEach { item ->
+                DropdownMenuItem(
+                    text = { GeneralNormalBlackText(value = stringResource(id = item.textResId)) },
+                    onClick = {
+                        selectedPeriod = item
+                        onItemSelected(item)
+                        expanded = false
+                    }
+                )
+            }
         }
     }
 }
