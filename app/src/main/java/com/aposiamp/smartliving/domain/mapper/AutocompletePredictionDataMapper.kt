e2ac1b9ad74fc4b3ca57f02be13cccec37e6882d
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-28 21:54:47 +0300

    Rename AutoCompletePrediction.kt to AutoCompletePredictionData.kt

--- app/src/main/java/com/aposiamp/smartliving/domain/mapper/AutocompletePredictionDataMapper.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/mapper/AutocompletePredictionDataMapper.kt
@@ -1,17 +1,17 @@
 package com.aposiamp.smartliving.domain.mapper
 
 import com.aposiamp.smartliving.data.model.AutoCompletePredictionDTO
-import com.aposiamp.smartliving.domain.model.AutoCompletePrediction
+import com.aposiamp.smartliving.domain.model.AutoCompletePredictionData
 
 object AutocompletePredictionDataMapper {
-    private fun toDomain(data: AutoCompletePredictionDTO): AutoCompletePrediction {
-        return AutoCompletePrediction(
+    private fun toDomain(data: AutoCompletePredictionDTO): AutoCompletePredictionData {
+        return AutoCompletePredictionData(
             placeId = data.placeId,
             fullAddress = data.fullAddress
         )
     }
 
-    fun toDomainList(dataList: List<AutoCompletePredictionDTO>): List<AutoCompletePrediction> {
+    fun toDomainList(dataList: List<AutoCompletePredictionDTO>): List<AutoCompletePredictionData> {
         return dataList.map { toDomain(it) }
     }
 }
