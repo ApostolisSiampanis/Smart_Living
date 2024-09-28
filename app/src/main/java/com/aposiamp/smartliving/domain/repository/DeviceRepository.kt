4a47146e9dfc212fa82d6864a5d333cdd95ead68
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-28 16:08:41 +0300

    Update updateDeviceState to return a nullable DeviceHistoryDTO into DeviceRepository

--- app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
+++ app/src/main/java/com/aposiamp/smartliving/domain/repository/DeviceRepository.kt
@@ -3,6 +3,7 @@ package com.aposiamp.smartliving.domain.repository
 import com.aposiamp.smartliving.data.model.AirConditionStatusDTO
 import com.aposiamp.smartliving.data.model.DehumidifierStatusDTO
 import com.aposiamp.smartliving.data.model.DeviceDataDTO
+import com.aposiamp.smartliving.data.model.DeviceHistoryDTO
 import com.aposiamp.smartliving.data.model.DeviceIdAndTypeDTO
 import com.aposiamp.smartliving.data.model.DeviceModeDTO
 import com.aposiamp.smartliving.data.model.DeviceStateDTO
@@ -15,6 +16,6 @@ interface DeviceRepository {
     suspend fun getThermostatStatus(deviceId: String): ThermostatStatusDTO?
     suspend fun getAirConditionStatus(deviceId: String): AirConditionStatusDTO?
     suspend fun getDehumidifierStatus(deviceId: String): DehumidifierStatusDTO?
-    suspend fun updateDeviceState(deviceId: String, deviceStateDTO: DeviceStateDTO): Boolean
+    suspend fun updateDeviceState(deviceId: String, deviceStateDTO: DeviceStateDTO): DeviceHistoryDTO?
     suspend fun updateDeviceMode(deviceId: String, deviceModeDTO: DeviceModeDTO): Boolean
 }
