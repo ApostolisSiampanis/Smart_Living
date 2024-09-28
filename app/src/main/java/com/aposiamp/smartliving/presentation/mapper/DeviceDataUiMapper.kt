eb1cc702bb6899a900390d40b76f889527bb7ee8
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-28 23:49:19 +0300

    Update DeviceDataUiMapper, add toSimpleDeviceDataUiModel and toSimpleDeviceDataUiModelList

--- app/src/main/java/com/aposiamp/smartliving/presentation/mapper/DeviceDataUiMapper.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/mapper/DeviceDataUiMapper.kt
@@ -2,6 +2,7 @@ package com.aposiamp.smartliving.presentation.mapper
 
 import com.aposiamp.smartliving.domain.model.DeviceData
 import com.aposiamp.smartliving.presentation.model.DeviceDataUiModel
+import com.aposiamp.smartliving.presentation.model.SimpleDeviceDataUiModel
 
 object DeviceDataUiMapper {
     private fun fromDomain(domain: DeviceData): DeviceDataUiModel {
@@ -14,5 +15,16 @@ object DeviceDataUiMapper {
 
     fun fromDomainList(domainList: List<DeviceData>): List<DeviceDataUiModel> {
         return domainList.map { fromDomain(it) }
+    }
+
+    private fun toSimpleDeviceDataUiModel(device: DeviceDataUiModel): SimpleDeviceDataUiModel {
+        return SimpleDeviceDataUiModel(
+            deviceId = device.deviceId,
+            deviceName = device.deviceName
+        )
+    }
+
+    fun toSimpleDeviceDataUiModelList(devices: List<DeviceDataUiModel>): List<SimpleDeviceDataUiModel> {
+        return devices.map { toSimpleDeviceDataUiModel(it) }
     }
 }
