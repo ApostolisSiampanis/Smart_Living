a701da588e39de3cae133b9d7c054ce5979bd809
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-29 13:47:27 +0300

    Update PieChartComponents.kt and in PieChart i pass the colors as a parameter

--- app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/PieChartComponents.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/ui/component/PieChartComponents.kt
@@ -12,11 +12,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -30,15 +27,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.aposiamp.smartliving.R
 
 @Composable
 fun PieChart(
     data: Map<String, Double>,
+    colors: List<Color>,
     radiusOuter: Dp = 90.dp,
     chartBarWidth: Dp = 20.dp,
     animDuration: Int = 1000
@@ -49,16 +45,6 @@ fun PieChart(
     data.values.forEachIndexed { index, values ->
         floatArcValue.add(index, 360 * values.toFloat() / totalSum.toFloat())
     }
-
-    // TODO: Remove this and pass the colors as a parameter and make it dynamic based on the data
-    val colors = listOf(
-        Color.Red,
-        Color.Blue,
-        Color.Green,
-        Color.Yellow,
-        Color.Cyan,
-        Color.Magenta
-    )
 
     var animationPlayed by remember { mutableStateOf(false) }
     var lastValue = 0f
@@ -80,7 +66,6 @@ fun PieChart(
         ), label = ""
     )
 
-    // TODO: Check it this is needed, if it recomposes multiple times
     LaunchedEffect(key1 = true) {
         animationPlayed = true
     }
