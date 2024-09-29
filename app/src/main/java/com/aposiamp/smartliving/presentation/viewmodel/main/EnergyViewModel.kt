dd53f49e76fcf4c5cd957c366a49163c863d819e
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-09-29 13:22:27 +0300

    Update EnergyViewModel, add GetPeriodDataUseCase, loading state and use the new use case with fetchPeriodData

--- app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/EnergyViewModel.kt
+++ app/src/main/java/com/aposiamp/smartliving/presentation/viewmodel/main/EnergyViewModel.kt
@@ -1,15 +1,40 @@
 package com.aposiamp.smartliving.presentation.viewmodel.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aposiamp.smartliving.domain.model.Period
+import com.aposiamp.smartliving.domain.usecase.main.GetPeriodDataUseCase
 import com.aposiamp.smartliving.domain.usecase.main.GetPeriodItemsUseCase
 import com.aposiamp.smartliving.presentation.mapper.PeriodUiMapper.toUiModelList
 import com.aposiamp.smartliving.presentation.model.PeriodItemUiModel
+import com.aposiamp.smartliving.presentation.model.SimpleDeviceDataUiModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class EnergyViewModel(
-    private val getPeriodItemsUseCase: GetPeriodItemsUseCase
+    private val getPeriodItemsUseCase: GetPeriodItemsUseCase,
+    private val getPeriodDataUseCase: GetPeriodDataUseCase
 ) : ViewModel() {
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    private val _periodData = MutableStateFlow<Map<String, Double>>(emptyMap())
+    val periodData: StateFlow<Map<String, Double>> get() = _periodData
+
     fun getPeriodItems(): List<PeriodItemUiModel> {
         val domainItems = getPeriodItemsUseCase.execute()
         return domainItems.toUiModelList()
+    }
+
+    fun fetchPeriodData(deviceList: List<SimpleDeviceDataUiModel>, period: Period) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val data = deviceList.associate { device ->
+                device.deviceId!! to getPeriodDataUseCase.execute(device.deviceId, period)
+            }
+            _periodData.value = data
+            _isLoading.value = false
+        }
     }
 }
